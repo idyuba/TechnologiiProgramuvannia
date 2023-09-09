13a285d86ecfe94f0fc7d33ebb918a6034432e5b
Ihor Diuba <idyuba@gmail.com>
2023-09-09 12:53:07 +0300

    change training section for quadratic equation

--- lectures/topic_01/trainingTopic_01/training05.py
+++ lectures/topic_01/trainingTopic_01/training05.py
@@ -1,22 +1,22 @@
 import math
 
 # aX**2 + bX + c = 0
-# sample  X**2 - 2X -3 = 0, x1 = 3, x2 = -1
+# D = b**2 - 4ac
+# D < 0 
+# D == 0
+# D > 0
+# x1 = (-b + sqrt(D))/2*a
+# x2 = (-b - sqrt(D))/2*a
 
-
+# sample  X**2 - 2X -3 = 0; x1 = 3, x2 = -1
 
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 c = int(input("Enter c: "))
 
-# D = b**2 - 4ac
-# D = 16
-
 D = b**2 - 4*a*c
-print("D = ", D)
 
-# x1 = (-b + sqrt(D))/2*a
-# x2 = (-b - sqrt(D))/2*a
+print("D = ", D)
 
 x1 = (-b + math.sqrt(D))/2*a
 x2 = (-b - math.sqrt(D))/2*a
