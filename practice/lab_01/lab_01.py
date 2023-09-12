36f6f363a716740f7b1c1b96145ff9b9532f988f
Ihor Diuba <idyuba@gmail.com>
2023-09-12 19:26:22 +0300

    add src file for lab_01

---
+++ practice/lab_01/lab_01.py
@@ -0,0 +1,76 @@
+
+## List [Item1, Item2, Item3]
+## Item {"name":"Jon", "phone":"0631234567"}
+
+# already sorted list
+list = [
+    {"name":"Bob", "phone":"0631234567"},
+    {"name":"Emma", "phone":"0631234567"},
+    {"name":"Jon",  "phone":"0631234567"},
+    {"name":"Zak",  "phone":"0631234567"}
+]
+
+def printAllList():
+    for elem in list:
+        strForPrint = "Student name is " + elem["name"] + ",  Phone is " + elem["phone"]
+        print(strForPrint)
+    return
+
+def addNewElement():
+    name = input("Pease enter student name: ")
+    phone = input("Please enter student phone: ")
+    newItem = {"name": name, "phone": phone}
+    # find insert position
+    insertPosition = 0
+    for item in list:
+        if name > item["name"]:
+            insertPosition += 1
+        else:
+            break
+    list.insert(insertPosition, newItem)
+    print("New element has been added")
+    return
+
+def deleteElement():
+    name = input("Please enter name to be delated: ")
+    deletePosition = -1
+    for item in list:
+        if name == item["name"]:
+            deletePosition = list.index(item)
+            break
+    if deletePosition == -1:
+        print("Element was not found")
+    else:
+        print("Dele position " + str(deletePosition))
+        list.pop(deletePosition)
+    return
+
+
+def updateElement():
+    name = input("Please enter name to be updated: ")
+    # implementation required
+
+def main():
+    while True:
+        chouse = input("Please specify the action [ C create, U update, D delete, P print,  X exit ] ")
+        match chouse:
+            case "C" | "c":
+                print("New element will be created:")
+                addNewElement()
+                printAllList()
+            case "U" | "u":
+                print("Existing element will be updated")
+            case "D" | "d":
+                print("Element will be deleted")
+                deleteElement()
+            case "P" | "p":
+                print("List will be printed")
+                printAllList()
+            case "X" | "x":
+                print("Exit()")
+                break
+            case _:
+                print("Wrong chouse")
+
+
+main()
