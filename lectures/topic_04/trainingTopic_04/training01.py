2acbe29c6f7c99ffa90131fd1686b26afd8012e4
Ihor Diuba <idyuba@gmail.com>
2023-10-05 00:20:32 +0300

    update topic 04

--- lectures/topic_04/trainingTopic_04/training01.py
+++ lectures/topic_04/trainingTopic_04/training01.py
@@ -8,10 +8,12 @@
 # runtime error
 # a = 1/0 # ZeroDivisionError
 # b = z + 7 # NameError
-c = "a" + 3 # TypeError
+# c = "a" + 3 # TypeError
 
-x = int(input("What x?: ")) # remember about int
-print(f"x is {x}") # remeber about f as format string   
+# x = int(input("What x?: ")) # remember about int
+# print(f"x is {x}") # remeber about f as format string 
+x = input("What x?: ")
+print(isinstance(x, str))  
 # what could be wrong?
 # check several numbers 0, -10, 50
 # qw -  invalid literal for int() with base 10: 'qw'
--- lectures/topic_04/trainingTopic_04/training04.py
+++ lectures/topic_04/trainingTopic_04/training04.py
@@ -1,9 +1,12 @@
 '''
+# try to do minimal work under try except
 try:
     x = int(input("What x?: "))
 except ValueError:
     print("x is not an integer")
 
+# Name erro is present
+# because of exception present x is undefined
 print(f"x is {x}") # NameError
 '''
 
